38ee0385862f94349115dc5d607118fbde09cbb7
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2022-11-01 03:16:09 -0400

    working

--- public/index.php
+++ public/index.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Kernel;
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG'], $context['APP_NAME']);
 };
